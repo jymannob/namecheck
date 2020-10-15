9cc738301868b0e1b7c6033502b8d3cd6a0246a3
jubobs <jubobs-oss@protonmail.com>
2020-10-15 14:48:13 +0200

    Use channel

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -9,36 +9,63 @@ import (
 	_ "github.com/jub0bs/namecheck/github"
 )
 
+type result struct {
+	username  string
+	platform  string
+	valid     bool
+	available bool
+	err       error
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: skdhsdkfjhsdf")
 		os.Exit(1)
 	}
 	username := os.Args[1]
+	ch := make(chan result)
 	var wg sync.WaitGroup
 	checkers := namecheck.Checkers()
 	wg.Add(len(checkers))
-	for _, c := range namecheck.Checkers() {
-		go check(c, username, &wg)
+	for _, c := range checkers {
+		go check(c, username, &wg, ch)
 	}
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for r := range ch {
+		fmt.Printf("%+v\n", r)
+	}
 }
 
-func check(c namecheck.Checker, username string, wg *sync.WaitGroup) {
+func check(
+	c namecheck.Checker,
+	username string,
+	wg *sync.WaitGroup,
+	ch chan<- result) {
 	defer wg.Done()
+	r := result{
+		username: username,
+		platform: c.String(),
+	}
 	valid := c.IsValid(username)
 	if !valid {
-		fmt.Printf("%q is invalid on %v\n", username, c)
+		ch <- r
 		return
 	}
+	r.valid = true
 	avail, err := c.IsAvailable(username)
 	if err != nil {
-		fmt.Println(err)
+		r.err = err
+		ch <- r
 		return
 	}
 	if !avail {
-		fmt.Printf("%q is unavailable on %v\n", username, c)
+		ch <- r
 		return
 	}
-	fmt.Printf("%q is available on %v\n", username, c)
+	r.available = true
+	ch <- r
 }
