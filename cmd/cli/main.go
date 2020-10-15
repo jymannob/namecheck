72567463f13eb9ed1a23bb38ac6c301ba1f77925
jubobs <jubobs-oss@protonmail.com>
2020-10-15 13:56:48 +0200

    Do things concurrently

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/jub0bs/namecheck"
 	_ "github.com/jub0bs/namecheck/github"
@@ -14,21 +15,30 @@ func main() {
 		os.Exit(1)
 	}
 	username := os.Args[1]
+	var wg sync.WaitGroup
+	checkers := namecheck.Checkers()
+	wg.Add(len(checkers))
 	for _, c := range namecheck.Checkers() {
-		valid := c.IsValid(username)
-		if !valid {
-			fmt.Printf("%q is invalid on %v\n", username, c)
-			continue
-		}
-		avail, err := c.IsAvailable(username)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if !avail {
-			fmt.Printf("%q is unavailable on %v\n", username, c)
-			continue
-		}
-		fmt.Printf("%q is available on %v\n", username, c)
+		go check(c, username, &wg)
 	}
+	wg.Wait()
+}
+
+func check(c namecheck.Checker, username string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	valid := c.IsValid(username)
+	if !valid {
+		fmt.Printf("%q is invalid on %v\n", username, c)
+		return
+	}
+	avail, err := c.IsAvailable(username)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if !avail {
+		fmt.Printf("%q is unavailable on %v\n", username, c)
+		return
+	}
+	fmt.Printf("%q is available on %v\n", username, c)
 }
